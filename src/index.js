eec97c6980d22637c5fc203fc2d50dd75d2cf694
winstoncwang <congwang@live.co.uk>
2020-05-17 13:07:21 +0100

    V0.131
    conditional rendering

--- src/index.js
+++ src/index.js
@@ -6,20 +6,29 @@ class App extends React.Component {
 	constructor (props) {
 		super(props); //reference to the parent component React.Component
 
-		this.state = { lat: null }; //state object
+		this.state = { lat: null, errorMessage: '' }; //initial state object METHOD ONE
 
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => {
 				// we cakked setState!!!!
 				this.setState({ lat: position.coords.latitude });
 			},
-			(error) => console.log(error)
+			(error) => {
+				this.setState({ errorMessage: error.message });
+			}
 		);
 	}
 
 	//REQUIRED render always!!!!
 	render () {
-		return <div>Lattitude: {this.state.lat}</div>;
+		//conditional rendering
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error Message: {this.state.errorMessage}</div>;
+		}
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div>Latitude: {this.state.lat}</div>;
+		}
+		return <div>Loading.....</div>;
 	}
 }
 
