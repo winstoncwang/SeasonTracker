f5b7d08ae6acb3e2d839f2b66bb8034292c9c616
winstoncwang <congwang@live.co.uk>
2020-05-17 11:54:48 +0100

    V0.12
    adding class based components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-	const currentPosition = window.navigator.geolocation.getCurrentPosition(
-		(position) => console.log(position),
-		(error) => console.log(error)
-	);
-	return <div>hello!</div>;
-};
+//class component
+class App extends React.Component {
+	render () {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => console.log(position),
+			(error) => console.log(error)
+		);
+
+		return <div>Lattitude: </div>;
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
