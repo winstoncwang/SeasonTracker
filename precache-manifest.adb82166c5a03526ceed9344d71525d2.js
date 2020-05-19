3c519911e199245771461de294f15a5db879b4fb
winstoncwang <congwang@live.co.uk>
2020-05-19 14:25:00 +0100

    Updates

---
+++ precache-manifest.adb82166c5a03526ceed9344d71525d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ab41755dafe005fe8758b80d67acd1",
+    "url": "/winstoncwang/SeasonTracker.git/index.html"
+  },
+  {
+    "revision": "42adfb0978a2532c7936",
+    "url": "/winstoncwang/SeasonTracker.git/static/css/main.558f4046.chunk.css"
+  },
+  {
+    "revision": "ec668b6deb9de122911e",
+    "url": "/winstoncwang/SeasonTracker.git/static/js/2.1ddb16c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/winstoncwang/SeasonTracker.git/static/js/2.1ddb16c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42adfb0978a2532c7936",
+    "url": "/winstoncwang/SeasonTracker.git/static/js/main.750d02a8.chunk.js"
+  },
+  {
+    "revision": "c43b121329ecf2db05c9",
+    "url": "/winstoncwang/SeasonTracker.git/static/js/runtime-main.40ce04f9.js"
+  }
+]);
